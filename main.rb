3bdb2e7b0df04a4bb64eda26859e5a36a0c3010d
mpataki <matpataki@gmail.com>
2013-05-08 21:15:33 -0400

    Added 2 strategies
    The solver can now solve puzzles using elimination of possibilities
    based on lines and 3x3 squares. This should solve most simple puzzles,
    but more strategies will be required in order to solve harder puzzles.

--- main.rb
+++ main.rb
@@ -10,7 +10,6 @@ class Cell
 
 	def value= value
 		@value = value
-		remove_possibility(value.to_i)
 	end
 
 	def initialize()
@@ -18,8 +17,12 @@ class Cell
 		@value = " "
 	end
 
+	# if only one possibility remains, return that value, otherwise return 0
 	def remove_possibility(k)
-		@possible_values.delete(k)
+		@possible_values.delete(k) 
+		if @possible_values.size == 1 && @value == " "
+			@possible_values.to_a[0]
+		else 0 end
 	end
 
 	def possible_values
@@ -49,6 +52,52 @@ class Board
 		}
 	end
 
+	def board
+		@board
+	end
+
+	def assign_value val, x, y
+		@board[x,y].value = val
+		#remove linear possibilities
+		0.upto(8){
+			|k|
+			val2 = @board[x, k].remove_possibility(val.to_i)
+			if val2 != 0
+				assign_value(val2, x, k)
+			end
+			val2 = @board[k, y].remove_possibility(val.to_i)
+			if val2 != 0
+				assign_value(val2, k, y)
+			end
+		}
+		#remove square possibilities
+		if x <= 2
+			xOffset = 0
+		elsif x <= 5
+			xOffset = 3
+		else
+			xOffset = 6
+		end
+		if y <= 2
+			yOffset = 0
+		elsif y <= 5
+			yOffset = 3
+		else
+			yOffset = 6
+		end
+		0.upto(2){
+			|xPos|
+			0.upto(2){
+				|yPos|
+				val2 = @board[xPos+xOffset, yPos+yOffset].remove_possibility(val.to_i)
+				if val2 != 0
+					assign_value(val2, xPos+xOffset, yPos+yOffset)
+				end
+			}
+		}
+
+	end
+
 	def read_board
 		x = 0
 		y = 0
@@ -56,7 +105,7 @@ class Board
 		STDIN.read.split(" " || "\n").each do |input|
 			i = input.to_i
 			if i >= 1 && i <= 9
-				@board[x,y].value = input
+				assign_value(input, x, y)
 			end
    			x==8 ? y=(y+1)%9 : nil
    			x = (x+1)%9 
@@ -68,29 +117,3 @@ end
 puzzle = Board.new
 puzzle.read_board
 puzzle.print_board
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
