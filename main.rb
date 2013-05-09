3ac3c8a50e866ce8729db95864c67720290ccf44
mpataki <matpataki@gmail.com>
2013-05-08 20:00:35 -0400

    Encapulate board functions into a class
    Moved board funcitons into a class in order to simplify the solving
    process to come.

--- main.rb
+++ main.rb
@@ -27,42 +27,47 @@ class Cell
 	end
 end
 
-def print_board board
-	0.upto(8) {
-		|y|
-		0.upto(8){
-			|x|
-			print board[x,y].value, " "
-			if x == 2 || x == 5
-				print "| "
+class Board 
+	def initialize()
+		@board = Matrix.build(9, 9) {Cell.new}
+	end
+
+	def print_board
+		0.upto(8) {
+			|y|
+			0.upto(8){
+				|x|
+				print @board[x,y].value, " "
+				if x == 2 || x == 5
+					print "| "
+				end
+			}
+			print "\n"
+			if y == 2 || y == 5
+				puts "---------------------"
 			end
 		}
-		print "\n"
-		if y == 2 || y == 5
-			puts "---------------------"
-		end
-	}
-end
+	end
 
-def read_board board
-	x = 0
-	y = 0
-	puts "Enter a puzzle"
-	STDIN.read.split(" " || "\n").each do |input|
-		i = input.to_i
-		if i >= 1 && i <= 9
-			board[x,y].value = input
+	def read_board
+		x = 0
+		y = 0
+		puts "Enter a puzzle"
+		STDIN.read.split(" " || "\n").each do |input|
+			i = input.to_i
+			if i >= 1 && i <= 9
+				@board[x,y].value = input
+			end
+   			x==8 ? y=(y+1)%9 : nil
+   			x = (x+1)%9 
 		end
-   		x==8 ? y=(y+1)%9 : nil
-   		x = (x+1)%9 
 	end
 end
 
 # MAIN PROGRAM
-board = Matrix.build(9, 9) {Cell.new}
-read_board board
-print_board(board)
-print board[0,0].possible_values, "\n"
+puzzle = Board.new
+puzzle.read_board
+puzzle.print_board
 
 
 
