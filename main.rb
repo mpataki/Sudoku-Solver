9ec376da0f2ce6f6122b2869e3b7c06d5bb0daca
mpataki <matpataki@gmail.com>
2013-05-31 02:14:08 -0400

    naked tuple block implementation

--- main.rb
+++ main.rb
@@ -209,7 +209,49 @@ class Board
 		}
 	end
 
-	def naked_pair_eliminate_col x, cands
+	def naked_tuple_eliminate_block x, y, cands
+		xOffset = find_offset(x)
+		yOffset = find_offset(y)
+		xOffset.upto(xOffset+2){
+			|x|
+			yOffset.upto(yOffset+2){
+				|y|
+				if @board[x,y].candidates != cands 
+					cands.each {
+						|cand|
+						@board[x,y].remove_candidate cand
+					}
+				end		
+			}
+		}
+	end
+
+	def naked_tuple_block x, y
+		xOffset = find_offset(x)
+		yOffset = find_offset(y)
+		xOffset.upto(xOffset+2){
+			|x|
+			yOffset.upto(yOffset+2){
+				|y|
+				cands = @board[x,y].candidates
+				counter = @board[x,y].candidates.size 
+				xOffset.upto(xOffset+2){
+					|x2|
+					yOffset.upto(yOffset+2){
+						|y2|
+						if @board[x2,y2].candidates == cands
+							counter = counter - 1
+							if counter == 0
+								naked_tuple_eliminate_block x2, y2, cands
+							end
+						end
+					}
+				}
+			}
+		}
+	end
+
+	def naked_tuple_eliminate_col x, cands
 		0.upto(8) {
 			|y|
 			if @board[x,y].candidates != cands 
@@ -221,7 +263,7 @@ class Board
 		}
 	end
 
-	def naked_pair_col x
+	def naked_tuple_col x
 		0.upto(7) {
 			|y|
 			cands = @board[x,y].candidates
@@ -231,14 +273,14 @@ class Board
 				if @board[x,y2].candidates == cands
 					counter = counter - 1
 					if counter == 0
-						naked_pair_eliminate_col x, cands
+						naked_tuple_eliminate_col x, cands
 					end
 				end
 			}
 		}
 	end
 
-	def naked_pair_eliminate_row y, cands
+	def naked_tuple_eliminate_row y, cands
 		0.upto(8) {
 			|x|
 			if @board[x,y].candidates != cands
@@ -250,7 +292,7 @@ class Board
 		}
 	end
 
-	def naked_pair_row y
+	def naked_tuple_row y
 		0.upto(7) {
 			|x|
 			cands = @board[x,y].candidates
@@ -260,7 +302,7 @@ class Board
 				if @board[x2,y].candidates == cands
 					counter = counter - 1
 					if counter == 0
-						naked_pair_eliminate_row y, cands
+						naked_tuple_eliminate_row y, cands
 					end
 				end
 			}
@@ -273,14 +315,15 @@ class Board
 			0.upto(2){
 				|y|
 				hidden_singles_block x*3, y*3
+				naked_tuple_block x*3, y*3
 			}
 		}
 		0.upto(8){
 			|k|
 			hidden_singles_row k
 			hidden_singles_col k
-			naked_pair_row k
-			naked_pair_col k
+			naked_tuple_row k
+			naked_tuple_col k
 		}
 	end
 
