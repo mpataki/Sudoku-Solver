d059b011e677b332745ae6d5bfd52f3e7fbcbe9a
mpataki <matpataki@gmail.com>
2013-07-06 00:19:08 -0400

    separating files

--- main.rb
+++ main.rb
@@ -2,6 +2,8 @@
 
 require 'matrix'
 require 'set'
+require './hiddenSingles.rb'
+require './nakedTuples.rb'
 
 class Cell
 	def value
@@ -128,188 +130,8 @@ class Board
 		}
 	end
 
-	# Uses the same technique as in the simpler row and column versions below
-	def hidden_singles_block x, y
-		xOffset = find_offset(x)
-		yOffset = find_offset(y)
-		arr = Array.new
-		xOffset.upto(xOffset+2){
-			|x|
-			yOffset.upto(yOffset+2){
-				|y|
-				if !@board[x,y].solved 
-					@board[x,y].candidates.each {
-						|elm|
-						if arr[elm] == nil
-							arr[elm] = [x,y]
-						else  
-							arr[elm] = -1
-						end
-					}
-				end
-			}
-		}
-		1.upto(9){
-			|i|
-			if arr[i] != nil && arr[i] != -1
-				assign_value(i, arr[i][0], arr[i][1])
-			end
-		}
-	end
-
-	# The array is used as follows: Each index position represents a possible value,
-	# and the value stored at that index is the y coordinate of where the value occurs.
-	# If a value exists as a candidate in more than one y coordinate, the index position
-	# will be set to -1 since we are only looking for singles. Once we have looked at the
-	# whole row any array position not equal to nil or -1 are singles, and can be assigned.
-	def hidden_singles_row y
-		arr = Array.new
-		0.upto(8) {
-			|x|
-			if !@board[x,y].solved
-				@board[x,y].candidates.each {
-					|elm|
-					if arr[elm] == nil
-						arr[elm] = x
-					else  
-						arr[elm] = -1
-					end
-				}
-			end
-		}
-		1.upto(9){
-			|i|
-			if arr[i] != nil && arr[i] != -1
-				assign_value(i, arr[i], y)
-			end
-		}
-	end
-
-	# Works the same as hidden_singles_row above.
-	def hidden_singles_col x
-		arr = Array.new
-		0.upto(8) {
-			|y|
-			if !@board[x,y].solved
-				@board[x,y].candidates.each {
-					|elm|
-					if arr[elm] == nil
-						arr[elm] = y
-					else  
-						arr[elm] = -1
-					end
-				}
-			end
-		}
-		1.upto(9){
-			|i|
-			if arr[i] != nil && arr[i] != -1
-				assign_value(i, x, arr[i])
-			end
-		}
-	end
-
-	def naked_tuple_eliminate_block x, y, cands
-		xOffset = find_offset(x)
-		yOffset = find_offset(y)
-		xOffset.upto(xOffset+2){
-			|x|
-			yOffset.upto(yOffset+2){
-				|y|
-				if @board[x,y].candidates != cands 
-					cands.each {
-						|cand|
-						@board[x,y].remove_candidate cand
-					}
-				end		
-			}
-		}
-	end
-
-	def naked_tuple_block x, y
-		xOffset = find_offset(x)
-		yOffset = find_offset(y)
-		xOffset.upto(xOffset+2){
-			|x|
-			yOffset.upto(yOffset+2){
-				|y|
-				cands = @board[x,y].candidates
-				counter = @board[x,y].candidates.size 
-				xOffset.upto(xOffset+2){
-					|x2|
-					yOffset.upto(yOffset+2){
-						|y2|
-						if @board[x2,y2].candidates == cands
-							counter = counter - 1
-							if counter == 0
-								naked_tuple_eliminate_block x2, y2, cands
-							end
-						end
-					}
-				}
-			}
-		}
-	end
-
-	def naked_tuple_eliminate_col x, cands
-		0.upto(8) {
-			|y|
-			if @board[x,y].candidates != cands 
-				cands.each {
-					|cand|
-					@board[x,y].remove_candidate cand
-				}
-			end
-		}
-	end
-
-	def naked_tuple_col x
-		0.upto(7) {
-			|y|
-			cands = @board[x,y].candidates
-			counter = @board[x,y].candidates.size - 1
-			(y+1).upto(7) {
-				|y2|
-				if @board[x,y2].candidates == cands
-					counter = counter - 1
-					if counter == 0
-						naked_tuple_eliminate_col x, cands
-					end
-				end
-			}
-		}
-	end
-
-	def naked_tuple_eliminate_row y, cands
-		0.upto(8) {
-			|x|
-			if @board[x,y].candidates != cands
-				cands.each {
-					|cand|
-					@board[x,y].remove_candidate cand
-				}
-			end
-		}
-	end
-
-	def naked_tuple_row y
-		0.upto(7) {
-			|x|
-			cands = @board[x,y].candidates
-			counter = @board[x,y].candidates.size - 1
-			(x+1).upto(7) {
-				|x2|
-				if @board[x2,y].candidates == cands
-					counter = counter - 1
-					if counter == 0
-						naked_tuple_eliminate_row y, cands
-					end
-				end
-			}
-		}
-	end
-
-	def find_HS_NP
+# Find hidden singles and naked pairs
+	def find_HS_NT
 		0.upto(2) {
 			|x|
 			0.upto(2){
@@ -361,9 +183,9 @@ end
 # MAIN PROGRAM
 puzzle = Board.new
 puzzle.read_board
-0.upto(100) {
+0.upto(300) {
 	puzzle.solved? ? break : nil
-	puzzle.find_HS_NP
+	puzzle.find_HS_NT
 }
 puzzle.print_board
 puzzle.print_possibilities
